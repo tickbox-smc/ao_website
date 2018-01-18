b0523e3a660441a6a6a0110e38910eb047f5fa40
Chris Falck <chris.falck@tickboxconsulting.com>
2018-01-18 16:04:20 +0000

    debugging

--- lib/puppet/type/http.rb
+++ lib/puppet/type/http.rb
@@ -1,5 +1,5 @@
 Puppet::Type.newtype(:http, :is_capability => true) do
-  newparam(:name, :namevar => true)
+  newparam(:name, :is_namevar => true)
   newparam(:http_name)
   newparam(:http_port)
   newparam(:http_ip)
